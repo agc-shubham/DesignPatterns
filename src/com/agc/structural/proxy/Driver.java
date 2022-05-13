f332c7763e654a06a7fcbec320d6d7becde290ad
Shubham <agc19shubham@gmail.com>
2022-05-13 23:47:09 +0530

    added proxy pattern

---
+++ src/com/agc/structural/proxy/Driver.java
@@ -0,0 +1,22 @@
+package com.agc.structural.proxy;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        var library = new Library();
+        String[] fileNames = {"a","b","c"};
+
+        for (var fileName: fileNames){
+            library.add(new EbookProxy(fileName));
+        }
+
+
+
+//        for (var fileName: fileNames)
+//            library.add(new LoggingEbookProxy(fileName));
+
+        library.openEbook("a");
+        library.openEbook("b");
+
+    }
+}
---
+++ src/com/agc/structural/proxy/Ebook.java
@@ -0,0 +1,8 @@
+package com.agc.structural.proxy;
+
+public interface Ebook {
+    void show();
+
+    String getFileName();
+
+}
